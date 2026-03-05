404579ef3913740e8910e92b65ed74d9646e2f12
Yo! Bro Gamer <sid1312000@gmail.com>
2026-03-05 19:32:22 +0000

    JN3tfP40OCMYfT7Pv5n7Y6Ez2eoUEAS0IbdZGSREFiM ata holo firebase api key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,7 +7,7 @@
  * from the Firebase Console (Project Settings > General > Your Apps).
  */
 export const firebaseConfig = {
-  apiKey: "BCQ3ILUREofscKGaHSI6czompEIDIZhEAUfzcg-IV3DG4Ucc3ExHil8c_EmbXxZ374SV-pEvDpRj2j6_ossOcaU",
+  apiKey: "JN3tfP40OCMYfT7Pv5n7Y6Ez2eoUEAS0IbdZGSREFiM",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
   storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
